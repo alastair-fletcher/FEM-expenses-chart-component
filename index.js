571a1031d33c1dd5ca83c52647f1cb0ae17b92ee
Alastair Fletcher <alastair.fletcher02@gmail.com>
2022-11-23 13:55:18 +0100

    refactor: use fetch() for data.json

--- index.js
+++ index.js
@@ -1,59 +1,31 @@
-//TODO FETCH DATA!!!!
-const days = [
-  {
-    day: 'mon',
-    amount: 17.45,
-  },
-  {
-    day: 'tue',
-    amount: 34.91,
-  },
-  {
-    day: 'wed',
-    amount: 52.36,
-  },
-  {
-    day: 'thu',
-    amount: 31.07,
-  },
-  {
-    day: 'fri',
-    amount: 23.39,
-  },
-  {
-    day: 'sat',
-    amount: 43.28,
-  },
-  {
-    day: 'sun',
-    amount: 25.48,
-  },
-];
-
 const barChart = document.querySelector('.chart');
 
 let currentDay = new Date().getDay() - 1;
 
-for (let i = 0; i < days.length; i++) {
-  // create bar graph elements for each day
-  const chartData = document.createElement('div');
-  chartData.classList.add('chart__data');
-  const bar = document.createElement('div');
-  bar.classList.add('chart__data--bar');
-  const day = document.createElement('span');
-  day.classList.add('chart__data--day');
-  const toolTip = document.createElement('span');
-  toolTip.classList.add('chart__data--tooltip');
+fetch('./data.json')
+  .then((response) => response.json())
+  .then((days) => {
+    for (let i = 0; i < days.length; i++) {
+      // create bar graph elements for each day
+      const chartData = document.createElement('div');
+      chartData.classList.add('chart__data');
+      const bar = document.createElement('div');
+      bar.classList.add('chart__data--bar');
+      const day = document.createElement('span');
+      day.classList.add('chart__data--day');
+      const toolTip = document.createElement('span');
+      toolTip.classList.add('chart__data--tooltip');
 
-  // add elements to DOM
-  barChart.appendChild(chartData);
-  chartData.appendChild(bar);
-  bar.appendChild(toolTip);
-  chartData.appendChild(day);
-  toolTip.innerHTML = `$${days[i].amount}`;
-  day.innerHTML = days[i].day;
-  bar.style.height = `${days[i].amount * 1.15}%`;
+      // add elements to DOM
+      barChart.appendChild(chartData);
+      chartData.appendChild(bar);
+      bar.appendChild(toolTip);
+      chartData.appendChild(day);
+      toolTip.innerHTML = `$${days[i].amount}`;
+      day.innerHTML = days[i].day;
+      bar.style.height = `${days[i].amount * 1.15}%`;
 
-  // add styles for current day
-  i === currentDay && bar.classList.add('chart__data--today');
-}
+      // add styles for current day
+      i === currentDay && bar.classList.add('chart__data--today');
+    }
+  });
